0ec07f884532fc6c491c49e4329f26f04c984e37
PepaFajkus <36948825+PepaFajkus@users.noreply.github.com>
2020-04-27 18:19:34 +0200

    změna buněk na tabulku

--- covid19/api.php
+++ covid19/api.php
@@ -1,56 +1,44 @@
-<?php
+<?php 
+
 include("tridy.php");
 
 $infected_people = new Covid_19();
 
 ?>
 
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cs">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- UIkit Styly -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/css/uikit.min.css" />
+
+<!-- UIkit Styly -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/css/uikit.min.css"/>
     <!-- UIkit Javascript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit-icons.min.js"></script>
-    <title>COVID-19</title>
-    <style> 
-        .Center { 
-            position: fixed; 
-            top: 30%; 
-            right: 20%;
-            left: 20%;
-        } 
-    </style> 
+
+    <title>korona</title>
 </head>
-
 <body>
-    <div class="uk-container">
-        <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match Center" uk-grid>
-            <div>
-                <div class="uk-card uk-card-default uk-card-body">
-                    <h3 style='text-align: center' class="uk-card-title">Celkový počet nakažených</h3>
-                    <p style='text-align: center'><?php echo $infected_people->total_infected() ?></p>
-                </div>
-            </div>
-            <div>
-                <div class="uk-card uk-card-default uk-card-body">
-                    <h3 style='text-align: center' class="uk-card-title">Podle pohlaví</h3>
-                    <p style='text-align: center'><?php echo $infected_people->sex_infected() ?></p>
-                </div>
-            </div>
-            <div>
-                <div class="uk-card uk-card-default uk-card-body">
-                    <h3 style='text-align: center' class="uk-card-title">Průměrný věk nakaženého</h3>
-                    <p style='text-align: center'><?php echo round($infected_people->average_age()) ?> let</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
 
+<table class="uk-table uk-table-divider">
+    <thead>
+        <tr>
+            <th>Celkový počet</th>
+            <th>Podle pohlaví</th>
+            <th>Průměrný věk</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><?php echo $infected_people->total_infected() ?></td>
+            <td><?php echo $infected_people->sex_infected() ?></td>
+            <td><?php echo $infected_people->average_age() ?></td>
+        </tr>
+    </tbody>
+</table>
+
+</body>
 </html>
